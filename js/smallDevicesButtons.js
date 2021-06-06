7a8b9c57d50a69cceb31355c7a6b9a00fbd78fb7
Daniela Caus <danicaus.br@gmail.com>
2021-06-06 19:04:29 -0300

    Add search animation for small devices

--- js/smallDevicesButtons.js
+++ js/smallDevicesButtons.js
@@ -1,8 +1,29 @@
 
 
 function toggleMenu() {
-    const divMenu = document.querySelector('[data-menu]');
+    const menu = document.querySelector('[data-menu]');
     const buttonClass = document.querySelector('.menuIcon__hamburger');
     buttonClass.classList.toggle('menuOpen');
-    divMenu.toggleAttribute('hidden');
+    menu.toggleAttribute('hidden');
+}
+
+function toggleSearch() {
+    const header = document.querySelector('.header')
+    const searchElement = document.querySelector('[data-search]');
+    const buttonClass = document.querySelector('.menuIcon__search');
+    const logoClass = document.querySelector('.logo-alura');
+    
+    const logoElement = document.createElement('a');
+    logoElement.classList.add('logo-alura');
+    
+    
+    buttonClass.classList.toggle('searchOpen');
+    if(buttonClass.classList.contains('searchOpen')) {
+        logoClass.remove();
+        searchElement.setAttribute("style", "visibility:visible;");
+    } else {
+        header.insertBefore(logoElement, searchElement);
+        searchElement.removeAttribute("style");
+    }
+    ;
 }
