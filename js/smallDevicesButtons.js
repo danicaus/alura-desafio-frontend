0bbe3dfda86dc5c44d296968908700e4956603a4
Daniela Caus <danicaus.br@gmail.com>
2021-06-06 16:09:59 -0300

    Changes hamburguer-menu and adds animations

--- js/smallDevicesButtons.js
+++ js/smallDevicesButtons.js
@@ -1,6 +1,8 @@
 
 
 function toggleMenu() {
-    const divMenu = document.querySelector('[data-menu]')
-    divMenu.toggleAttribute('hidden')
+    const divMenu = document.querySelector('[data-menu]');
+    const buttonClass = document.querySelector('.menuIcon__hamburger');
+    buttonClass.classList.toggle('menuOpen');
+    divMenu.toggleAttribute('hidden');
 }
