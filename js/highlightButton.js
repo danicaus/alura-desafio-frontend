039371de213b856baddafd2208f2111d9eb0fca3
Daniela Caus <danicaus.br@gmail.com>
2021-06-07 16:47:13 -0300

    Fix line-break problem on codeArea

--- js/highlightButton.js
+++ js/highlightButton.js
@@ -6,7 +6,6 @@ for (const option of document.querySelectorAll(".customSelectOption")) {
         const highlightLang = document.querySelector('.code-textarea')
         const languageSelected = document.querySelector('.-csSelected');
         const language = languageSelected.getAttribute('data-value');
-        console.log(language)
 
         if(!highlightLang.classList.contains(language)){
             const languages = ["javascript", "html", "css"];
@@ -21,26 +20,24 @@ for (const option of document.querySelectorAll(".customSelectOption")) {
 highlightButton.addEventListener('click', (event)=> {
     event.preventDefault();
     const code = document.querySelector('code')
-    hljs.highlightElement(code);
+    const languageSelected = document.querySelector('.-csSelected');
+    const language = languageSelected.getAttribute('data-value');
+    code.innerHTML = hljs.highlight(code.innerText, {language: language}).value;
 })
 
-// const areaDoCodigo = document.querySelector('.code-wrapper')
-// const languageSelected = document.querySelector('.-csSelected');
-// const language = languageSelected.getAttribute('data-value');
-// const botao = document.querySelector('.code-btnHighlight')
-
-// function mudaLinguagem() {
-//     const codigo = areaDoCodigo.querySelector('code')
-//     areaDoCodigo.innerHTML = `<code class="code-textarea hljs ${language}" spellcheck="false" contenteditable="true" aria-label="editor"></code>`
-//     areaDoCodigo.firstChild.innerText = codigo.innerText
-// }
-
-// languageSelected.addEventListener('click', () => {
-//     mudaLinguagem()
-// })
-
-// botao.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     const codigo = areaDoCodigo.querySelector('code')
-//     hljs.highlightBlock(codigo)
-// })
+/*Meu código HTML para teste
+ <!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Desafio FrontEnd - Alura Dev</title>
+    </head>
+    <body>
+        <h1>Olá, mundo!</h1>
+        <h2>Essa é a estrutura responsiva do Alura Dev. Vai ficar bem bonitinho!</h2>
+        <p>Me segue no GitHub!<p>
+            <a href="https://github.com/danicaus">https://github.com/danicaus</a>
+    </body>
+</html>
+*/
